09966ca39b37544b101bf0ac1336bfe122124010
Nikita <132465691+nikitampmaurya@users.noreply.github.com>
2024-10-17 08:10:03 +0100

    Update WEEK2_exercise.R

--- WEEK2_exercise.R
+++ WEEK2_exercise.R
@@ -1,3 +1,7 @@
+# Q.1. The file caffeine.txt contains the values of the urinary metabolic ratio of 5–acetylamino–6–formylamino–3–
+# methyluracil to 1–methylxanthine (AFMU/1X) after oral administration of caffeine. Plot a histogram of the
+# data and comment on its distribution.
+
 library(tidyverse)
 
 df = read.table("caffeine.txt", header=T)
@@ -10,11 +14,14 @@ g1 + geom_histogram() +
   xlab("Enzyme ratio") +
   theme_bw()
 
-#according to the graph, if you see the x axis there is high variability in enzyme ratios,
+#according to the graph, if you see the x-axis there is high variability in enzyme ratios,
 #moreover, data is not evenly distributed (it is not symmetrical)
-#there are more count and higher peaks towards left so it is a left skewed distribution.
-#there are outliers towards right end.
+#there are more counts and higher peaks towards the left so it is a left skewed distribution.
+#there are outliers towards the right end.
 
+# Q2. The file pancreatic.csv contains the concentrations of 2 biomarkers, CA19-9 and CA125 (in U/ml), in the control
+# (healthy) and diseased (diagnosed with pancreatic cancer) individuals. Plot these data to investigate the
+# relationship of each biomarker to disease state.
 
 df1 = read.csv("pancreatic.csv", header=T)
 
@@ -29,15 +36,15 @@ g2 + geom_point()+
   facet_wrap(vars(status))
 
 
-#according to the graph, the graph representing diease group is more scatter than control group.
+#according to the graph, the graph representing the disease group is more scattered than control group.
 
-#in the dieases group, the expression of both biomarkers has tremendously increased. 
+#in the disease group, the expression of both biomarkers has tremendously increased. 
 
 #in the control group, the expression of CA125 is more than CA19.9, 
 
-#in the dieases group, the expression of CA19.9 is sligthly more than CA125
+#in the diseases group, the expression of CA19.9 is slightly more than CA125
 
-#whereas in controlled state, the expression of both the biomarkers is lesser ,  biomarker CA125 is more expressed in control.
+#whereas in a controlled state, the expression of both the biomarkers is lesser,  biomarker CA125 is more expressed in control.
 
 gg = ggplot(data=df1,aes(x=status,y=df1$CA19.9,color=status))
 
@@ -51,6 +58,13 @@ ggg + geom_boxplot() +
   scale_y_log10() +
   theme_bw()
 
+
+# Q.3. The file plant_height.csv contains data on the heights of different plants, plus various ecological factors from
+# where grow. First, produce a plot to compare the height (in metres) of different growthforms (herb, shrub or tree).
+# Second, produce a plot of how the height of these different growthforms varies with rain (the column ‘rain’, in
+# mm/yr). Label the axes appropriately. What does geom_smooth() do? Try adding it to your plot.
+
+
 df2 = read.csv('plant_height.csv', header = T)
 
 View(df2)
@@ -62,8 +76,8 @@ g3 + geom_boxplot() +
   scale_y_log10() +
   theme_bw(base_size = 16)
 
-#according to the boxplot, all trees taller and their height is highly variable, whereas 
-#herbs are smaller than shurb.
+#according to the boxplot, all trees are taller and their height is highly variable, whereas 
+#herbs are smaller than shrub.
 
 g4 = ggplot(data=df2,aes(x = rain, y = height, color = growthform))
 
